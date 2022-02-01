9a9c047060e430923b3b6c6632e4f1c053b6fb79
Stuart <86697483+Pilot67@users.noreply.github.com>
2022-02-01 14:52:38 +1100

    debug heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./Schemas");
 const { authMiddleware } = require("./utils/auth");
 
-(async function() {
+(async function () {
   const app = express();
   const PORT = process.env.PORT || 3001;
   const server = new ApolloServer({
@@ -12,26 +12,26 @@ const { authMiddleware } = require("./utils/auth");
     resolvers,
     context: authMiddleware,
   });
-  await server.start()
+  await server.start();
   server.applyMiddleware({ app, path: "/graphql" });
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  
+
   // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   sequelize.sync().then(() => {
     app.listen(PORT, () => {
       console.log("Now listening");
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
     });
   });
 })();
